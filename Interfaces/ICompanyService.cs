88f208739fd22466f623e303d8dedde4937da0f4
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-20 17:10:02 +0100

    feat(interfaces): create ICompanyService interface

---
+++ Interfaces/ICompanyService.cs
@@ -0,0 +1,10 @@
+using JobTracker.DTOs.Company;
+
+namespace JobTracker.Interfaces
+{
+  public interface ICompanyService
+  {
+    Task<CompanyResponseDto> GetByIdAsync(int id);
+    Task<CompanyResponseDto> CreateAsync(CreateCompanyDto dto);
+  }
+}
