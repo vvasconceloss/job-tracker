ca87895262f47b81ee87050aba9167ddaf56d596
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-20 16:27:38 +0100

    feat(data): register Company in DbContext with entity config

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -1,12 +1,31 @@
+using JobTracker.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobTracker.Data
 {
   public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
   {
+    public required DbSet<Company> Companies { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Company>(entity =>
+      {
+        entity.HasKey(c => c.Id);
+
+        entity.Property(c => c.Id)
+          .ValueGeneratedOnAdd();
+
+        entity.HasIndex(c => c.Name)
+          .IsUnique();
+
+        entity.Property(e => e.CreatedAt)
+          .HasDefaultValueSql("GETUTCDATE()");
+          
+        entity.Property(e => e.UpdatedAt)
+          .HasDefaultValueSql("GETUTCDATE()"); 
+      });
     }
   }
 }
