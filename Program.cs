28f621eaf90be064a87087c0a1acd77518654cde
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-20 21:05:00 +0100

    feat(api): wire controllers, service DI and Scalar UI in Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using JobTracker.Data;
+using Scalar.AspNetCore;
+using JobTracker.Services;
+using JobTracker.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,15 +12,21 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<ICompanyService, CompanyService>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
