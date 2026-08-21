c684629e37ddccd772d2e6f11f9ce40000875f40
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-21 13:04:55 +0100

    feat(enums): add ApplicationStatus enum

---
+++ Enums/ApplicationStatus.cs
@@ -0,0 +1,14 @@
+namespace JobTracker.Enums
+{
+  public enum ApplicationStatus
+  {
+    Interested,
+    Applied,
+    Screening,
+    Interview,
+    TechnicalTest,
+    Offer,
+    Rejected,
+    Withdrawn
+  }
+}
